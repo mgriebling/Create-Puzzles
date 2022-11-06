218215f5608c8b5a1b41f7e17fd0dc444063d09e
Michael Griebling <sam@c-inspirations.com>
2022-11-06 16:04:32 -0500

    Initial Commit

---
+++ Create Puzzles/Create_PuzzlesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Create_PuzzlesApp.swift
+//  Create Puzzles
+//
+//  Created by Mike Griebling on 2022-11-06.
+//
+
+import SwiftUI
+
+@main
+struct Create_PuzzlesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
